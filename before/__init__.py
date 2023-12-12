70150b62b5751ec8efe60baa327bd5e077e9d5e6
Tom Herman <tomherman4@gmail.com>
2023-12-12 18:23:39 +0200

    Initial commit - implemented and tested

---
+++ before/__init__.py
@@ -0,0 +1,78 @@
+from functools import reduce
+from importlib import reload
+from sys import intern
+
+
+xrange = range
+raw_input = input
+izip = zip
+
+
+_new_map = map
+_new_filter = filter
+_new_round = round
+
+
+def map(*args, **kwargs):
+    return list(_new_map(*args, **kwargs))
+
+
+def filter(*args, **kwargs):
+    return list(_new_filter(*args, **kwargs))
+
+
+def round(number, ndigits=0):
+    res = _new_round(number, ndigits)
+    res_plus1 = _new_round(number + 1, ndigits)
+    if res_plus1 - res == 2:  # This is the case that number == 2n+0.5
+        return res + 1
+    return res
+
+
+def range(*args, **kwargs):
+    return list(xrange(*args, **kwargs))
+
+
+def input(*args, **kwargs):
+    return eval(raw_input(*args, **kwargs))
+
+
+def zip(*args, **kwargs):
+    return list(izip(*args, **kwargs))
+
+
+def cmp(x, y):
+    return (x > y) - (x < y)
+
+
+def apply(function, *args, **kwargs):
+    return function(*args, **kwargs)
+
+
+def execfile(filename: str, globals=None, locals=None):
+    with open(filename, encoding='utf-8') as f:
+        code = f.read()
+    return exec(compile(code, filename, 'exec'), globals, locals)
+
+
+__all__ = [
+    "reduce",
+    "reload",
+    "intern",
+
+    "xrange",
+    "range",
+    "raw_input",
+    "input",
+    "izip",
+    "zip",
+
+    "map",
+    "filter",
+    "round",
+
+    "cmp",
+    "apply",
+    "execfile",
+]
+
---
+++ tests/test_before.py
@@ -0,0 +1,138 @@
+import operator
+from pathlib import Path
+
+from before import (
+    reduce, reload, intern,
+    xrange, range, raw_input, input, izip, zip,
+    map, filter, round,
+    cmp, apply, execfile,
+)
+
+from tempfile import TemporaryDirectory
+import sys
+from io import StringIO
+from string import printable
+
+
+def test_reduce():
+    assert reduce(operator.add, range(2, 10)) == 44
+
+
+def test_intern():
+    a = printable + 'cfgdsfcsvddgx'
+    b = printable + 'cfgdsfc' + 'svddgx'
+    assert a == b
+    assert a is not b
+    assert b is not a
+
+    a = intern(a)
+    b = intern(b)
+    assert a == b
+    assert a is b
+    assert b is a
+
+
+def test_reload(capsys):
+    with TemporaryDirectory() as dir:
+        sys.path.append(dir)
+        path = Path(dir) / 'ggggg.py'
+
+        with path.open('w') as f:
+            f.write('print(7)\n')
+
+        import ggggg
+        assert capsys.readouterr().out == "7\n"
+        for _ in range(10):
+            import ggggg
+            assert capsys.readouterr().out == ""
+
+        reload(ggggg)
+        import ggggg
+        assert capsys.readouterr().out == "7\n"
+        import ggggg
+        assert capsys.readouterr().out == ""
+
+        for _ in range(10):
+            reload(ggggg)
+            import ggggg
+            assert capsys.readouterr().out == "7\n"
+
+
+def test_xrange():
+    result = xrange(10)
+    assert not isinstance(result, list)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == list(result)
+
+
+def test_range():
+    result = range(10)
+    assert isinstance(result, list)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == result
+
+
+def test_raw_input():
+    sys.stdin = StringIO("'hello!'\n(lambda x:x+15)(2)\n3\n4+5\n")
+    assert raw_input() == "'hello!'"
+    assert raw_input() == '(lambda x:x+15)(2)'
+    assert raw_input() == '3'
+    assert raw_input() == '4+5'
+
+
+def test_input():
+    sys.stdin = StringIO("'hello!'\n(lambda x:x+15)(2)\n3\n4+5\n")
+    assert input() == 'hello!'
+    assert input() == 17
+    assert input() == 3
+    assert input() == 9
+
+
+def test_izip():
+    result = izip((1, 100), (2, 200), (3, 300))
+    assert not isinstance(result, list)
+    assert [(1, 2, 3), (100, 200, 300)] == list(result)
+
+
+def test_zip():
+    result = zip((1, 100), (2, 200), (3, 300))
+    assert isinstance(result, list)
+    assert [(1, 2, 3), (100, 200, 300)] == result
+
+
+def test_map():
+    assert [0, 2, 4, 6, 8] == map(lambda x: x * 2, range(5))
+
+
+def test_filter():
+    assert [0, 3, 6, 9] == filter(lambda x: x % 3 == 0, range(10))
+
+
+def test_round():
+    assert round(10) == 10
+    assert round(10.0) == 10
+    assert round(10.3) == 10
+    assert round(10.5) == 11
+    assert round(10.7) == 11
+    assert round(11.5) == 12
+
+
+def test_cmp():
+    assert cmp(1, 2) == -1
+    assert cmp(2, 1) == 1
+    assert cmp(2, 2) == 0
+    assert cmp('chicken', 'egg') == -1
+    assert cmp('egg', 'chicken') == 1
+    assert cmp('chicken', 'chicken') == 0
+
+
+def test_apply():
+    assert apply(max, 1, 2, 3, 7, 1, 5, 9, 3, 2) == 9
+
+
+def test_execfile(capsys):
+    with TemporaryDirectory() as dir:
+        path = Path(dir) / 'file.py'
+        with path.open('w') as f:
+            f.write('print(4+8, 3)\nprint(list(range(10)))\n')
+        execfile(str(path))
+
+        assert capsys.readouterr().out == "12 3\n[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\n"
